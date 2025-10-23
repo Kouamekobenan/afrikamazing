62eadeff555cdd2fef6c288ad34d5d16a549db5d
Nelson Kouame <kouamenelson47@gmail.com>
2025-10-23 03:20:29 +0200

    blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+// src/app/about/page.tsx
+export default function BlogPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">À propos d’Afrikamazing</h1>
+      <p className="mt-2 text-gray-600">
+        Bienvenue sur Afrikamazing — votre vitrine du savoir-faire africain !
+      </p>
+    </div>
+  );
+}
